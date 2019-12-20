dd4211bb5c4f036bb9d9556bba98b4f0c9ac43f7
Ashutosh Satapathy <48993028+RonAshu@users.noreply.github.com>
2019-12-20 12:24:28 +0530

    Delete madlibs.py

--- madlibs.py
+++
@@ -1,8 +0,0 @@
-name=input("Enter the name:")
-feel=input("LOVE or HATE:")
-feel1=input("DITCH or HURT:")
-celebrity=input("SAM OR RON:")
-print("Hello " + name )
-print("DO  you know how much i " +feel + " you?")
-print("Please do not "+ feel1 + " me!")
-print("I love " + celebrity)
